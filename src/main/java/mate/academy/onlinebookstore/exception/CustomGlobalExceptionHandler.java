1bfc9cbf18b440ddded3d7be4ac8615070f465ad
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-11-25 17:34:32 +0100

    Docker
    - Added Docker:
    -- docker-compose.yml
    -- .env
    -- Dockerfile

--- src/main/java/mate/academy/onlinebookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/onlinebookstore/exception/CustomGlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -39,7 +40,8 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
 
     @ExceptionHandler(value = {NoSuchElementException.class })
     protected ResponseEntity<Object> handleNoSuchElementException(RuntimeException exc) {
-        NoSuchElementException noSuchElementException = (NoSuchElementException) exc;
+        InternalAuthenticationServiceException noSuchElementException =
+                (InternalAuthenticationServiceException) exc;
         HttpStatusCode statusCode = HttpStatus.NOT_FOUND;
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
